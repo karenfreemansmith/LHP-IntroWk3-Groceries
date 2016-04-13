d5042c6ad2e4a1257a0ca164edc5056b7e8ad2ba
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-12 18:12:49 -0700

    add button to show list, function displays all items in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
 var groceries=[];
+
 $("#addIt").submit(function(event) {
   var groceryItem = $("input#item").val();
   var groceryAmount = $("input#amount").val();
 
   groceries.push([groceryAmount, groceryItem]);
-  $("#theList").text("(" + groceries[0][0] + ") "+ groceries[0][1]);
-  
   event.preventDefault();
+});
+
+$("#showList").click(function() {
+  groceries.forEach(function(grocery) {
+    $("#theList").append("<li>(" + grocery[0] + ") "+ grocery[1] + "</li>");
+  });
 });
