fe938dcff55266e6981100824f1b18587c384097
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-12 18:03:22 -0700

    add script to collect form data, insert into array and display on web page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+var groceries=[];
+$("#addIt").submit(function(event) {
+  var groceryItem = $("input#item").val();
+  var groceryAmount = $("input#amount").val();
 
-$("#addToList").submit(function(event) {
-  event.PreventDefault();
-  alert("Adding to list");
+  groceries.push([groceryAmount, groceryItem]);
+  $("#theList").text("(" + groceries[0][0] + ") "+ groceries[0][1]);
+  
+  event.preventDefault();
 });
