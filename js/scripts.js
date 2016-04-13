6ae4e62ef954c833c6905f03a28f4551b693cc6c
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-12 17:39:00 -0700

    layout basic page with form and space to display list

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+
+$("#addToList").submit(function(event) {
+  event.PreventDefault();
+  alert("Adding to list");
+});
