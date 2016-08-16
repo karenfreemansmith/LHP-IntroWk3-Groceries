eecc53a53e21373d1d2d74eff4759ebd05586b3e
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-08-15 17:51:55 -0700

    improve look of form, remove show/hide stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,16 @@
-//alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
 var groceries=[];
 
 $("#addIt").submit(function(event) {
   var groceryItem = $("input#item").val();
   var groceryAmount = $("input#amount").val();
+  event.preventDefault();
 
   groceries.push([groceryItem, groceryAmount]);
-  event.preventDefault();
-});
-
-$("#showList").click(function() {
   $("#theList").empty();
-  /* Map functin returns strange results "A/B" for all items???
-  var myGroceries=groceries.map(function(item){
-    return item[1].toUpperCase();
-  });
-  */
   groceries.sort();
   groceries.forEach(function(item) {
     $("#theList").append("<li>(" + item[1] + ") "+ item[0].toUpperCase() + "</li>");
   });
-  $("#myList").toggle();
-  $("#moreStuff").toggle();
-});
-
-$("#hideList").click(function() {
-  $("#myList").toggle();
-  $("#moreStuff").toggle();
+  $("input#item").val()="";
+  $("input#amount").val()="";
 });
